5d4d4eea8e70d1f0e9f40a9191a831f91109c052
halposhaven <matt.mattjones@gmail.com>
2015-05-11 22:26:54 +0000

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cleanslate::Application.config.secret_token = '739eeeec52966255de67612858172e58f525fef48af3b12bb4cc36c0df82e3f73becdbf276a1ef5a4fc729fc72aaa8aab763092bfe11e82ecdc5820369f954e9'
